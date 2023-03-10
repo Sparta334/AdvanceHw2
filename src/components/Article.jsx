05fb3b8b51e6340f47d323dedeed51c95cececbe
Sparta334 <ianmina0322@gmail.com>
2023-03-10 17:28:20 +0800

    tiny update 2

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -14,16 +14,16 @@ export default function Article(){
                 <hr className="divider--light"/>
                 </Col>
                 <p className="description__content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum
-                    corrupti ut iure aliquam. Quod, perferendis. Libero tempore iste, animi sint numquam illum quasi laboriosam
-                    unde pariatur quidem! Suscipit consequatur nihil dolor impedit temporibus ad cum, voluptatum odit ratione
-                    ullam
-                    eligendi amet beatae aliquid eaque nesciunt optio nobis ducimus itaque. Voluptatibus aspernatur assumenda
-                    quod
-                    quo error consequuntur, vero iusto reprehenderit repellat maiores, sapiente dolores voluptates minima. Alias
-                    dolore assumenda fuga quae repellat voluptatum quo, saepe exercitationem perspiciatis reiciendis modi
-                    tempora.
-                    Recusandae explicabo iusto corporis nam numquam dignissimos, necessitatibus, iure non culpa repellat
-                    repudiandae temporibus! Saepe excepturi tempore iusto eos sit!</p>
+                    corrupti ut iure aliquam.I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy 
+                    I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy
+                    I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy 
+                    I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy 
+                    I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy 
+                    I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy 
+                    I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy 
+                    I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy 
+                    I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy I am sexy guy 
+                    I am sexy guy I am sexy guy  HAHAAHHHHHHHHHHHHHHHHHHHAHHAHAHAAHAH</p>
 
             </Row>
             </Col> 
